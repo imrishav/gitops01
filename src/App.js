d65e2a50f6a92fc2295e30b98879de1d500faa7a
Rishav Sinha <rishav@live.co.uk>
2022-12-25 02:00:31 +0530

    inital Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <div style={{ fontSize: 30 }}>
+          Version <span style={{ color: 'red' }}>Initial</span>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
